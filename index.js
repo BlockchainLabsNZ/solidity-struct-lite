e40ff9a1573ac116d346f9b6aaa068c50d36b05a
Klaus Hott Vidal <klahott@gmail.com>
2018-05-16 16:03:14 +1200

    inntroducing handlebars

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const SolidityParser = require("solidity-parser");
 const Mustache = require("mustache");
+const Handlebars = require("handlebars");
 const BigNumber = require("bignumber.js");
 const _ = require("lodash");
 const byte_length = require("./src/constants.js");
@@ -31,9 +32,30 @@ const fetchStructs = function(statement) {
 // const tree = SolidityParser.parseFile("./contracts/StructLitePOC.sol");
 // let structs = fetchStructs(tree);
 
+var library = Handlebars.compile(
+  fs.readFileSync("./src/views/library.mustache").toString()
+);
+var variable_declaration = Handlebars.compile(
+  fs.readFileSync("./src/views/variable_declaration.mustache").toString()
+);
+var variable_getter = Handlebars.compile(
+  fs.readFileSync("./src/views/variable_getter.mustache").toString()
+);
+var first_variable_setter = Handlebars.compile(
+  fs.readFileSync("./src/views/first_variable_setter.mustache").toString()
+);
+var variable_setter = Handlebars.compile(
+  fs.readFileSync("./src/views/variable_setter.mustache").toString()
+);
+var bool_getter = Handlebars.compile(
+  fs.readFileSync("./src/views/bool_getter.mustache").toString()
+);
+var bool_setter = Handlebars.compile(
+  fs.readFileSync("./src/views/bool_setter.mustache").toString()
+);
+
 structs.forEach(struct => {
-  template = fs.readFileSync("./src/views/library.mustache");
-  let output = Mustache.render(template.toString(), struct);
+  let output = library(struct);
 
   console.log(output);
 
