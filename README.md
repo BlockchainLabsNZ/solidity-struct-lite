be09441b22c024539123d1b5b0f21d4e01f84af7
Klaus Hott Vidal <klahott@gmail.com>
2018-06-05 14:14:15 +1200

    Small work on the Readme and adding test structs to check that every type is supported.

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 # StructLite
 
-The solidity compiler tries it's best, but let's help it out a little. This library saves you gas when you are using structs that require more read access than write access.
+The solidity compiler tries it's best, but let's help it out a little. This library saves you gas when you are using structs that require more write access than read access and cases where you would call functions with multiple parameters and actual space can be saved by compacting the data.
+
+It implements a technique called **byte packing** and abstracts all of the math in a EVM optimized way.
 
 ## Getting Started
 
 ### Prerequisites
 
-- A solidity contract that makes use of the `struct` type
+* A solidity contract that makes use of the `struct` type
 
 ### Installation
 
@@ -24,7 +26,24 @@ node index.js ExampleContract.sol
 
 ## Running the tests
 
+First create a build for the struct you'd like to optimize for writing.
+
 ```bash
-npm test
+node index.js ExampleContract.sol
 ```
 
+Then run the generated tests on the build folder
+
+```bash
+cd build
+truffle test
+```
+
+The generated tests consist on possible usages of the struct and there are also some gas usage comparisons with a normal struct approach.
+
+## TODO
+
+* Make better report on gas usage comparisons.
+* Booleans should be packed as a bit, not a byte.
+* At the moment this project provides writing _1_ variable or _all_ of them. There must be an option for the developer to request a method to write a subset of variables.
+* Add specific struct configurations for edge cases in the code to check that changes don't break compatibility.
---
+++ contracts/ExtensiveTests.sol
@@ -0,0 +1,162 @@
+pragma solidity 0.4.23;
+
+
+contract ExtensiveTests {
+    struct Struct1 {
+        bool testingBool;
+        address testingAddress;
+        bytes1 testingBytes1;
+        bytes2 testingBytes2;
+        bytes3 testingBytes3;
+    }
+
+    struct Struct2 {
+        bytes4 testingBytes4;
+        bytes5 testingBytes5;
+        bytes6 testingBytes6;
+        bytes7 testingBytes7;
+        bytes8 testingBytes8;
+    }
+
+    struct Struct3 {
+        bytes9 testingBytes9;
+        bytes10 testingBytes10;
+        bytes11 testingBytes11;
+        bytes12 testingBytes12;
+        bytes13 testingBytes13;
+    }
+
+    struct Struct4 {
+        bytes14 testingBytes14;
+        bytes15 testingBytes15;
+        bytes16 testingBytes16;
+        bytes17 testingBytes17;
+        bytes18 testingBytes18;
+    }
+
+    struct Struct5 {
+        bytes19 testingBytes19;
+        bytes20 testingBytes20;
+        bytes21 testingBytes21;
+        bytes22 testingBytes22;
+        bytes23 testingBytes23;
+    }
+
+    struct Struct6 {
+        bytes24 testingBytes24;
+        bytes25 testingBytes25;
+        bytes26 testingBytes26;
+        bytes27 testingBytes27;
+        bytes28 testingBytes28;
+    }
+
+    struct Struct7 {
+        bytes29 testingBytes29;
+        bytes30 testingBytes30;
+        bytes31 testingBytes31;
+        bytes32 testingBytes32;
+        uint8 testingUint8;
+    }
+
+    struct Struct8 {
+        uint16 testingUint16;
+        uint24 testingUint24;
+        uint32 testingUint32;
+        uint40 testingUint40;
+        uint48 testingUint48;
+    }
+
+    struct Struct9 {
+        uint56 testingUint56;
+        uint64 testingUint64;
+        uint72 testingUint72;
+        uint80 testingUint80;
+        uint88 testingUint88;
+    }
+
+    struct Struct10 {
+        uint96 testingUint96;
+        uint104 testingUint104;
+        uint112 testingUint112;
+        uint120 testingUint120;
+        uint128 testingUint128;
+    }
+
+    struct Struct11 {
+        uint136 testingUint136;
+        uint144 testingUint144;
+        uint152 testingUint152;
+        uint160 testingUint160;
+        uint168 testingUint168;
+    }
+
+    struct Struct12 {
+        uint176 testingUint176;
+        uint184 testingUint184;
+        uint192 testingUint192;
+        uint200 testingUint200;
+        uint208 testingUint208;
+    }
+
+    struct Struct13 {
+        uint216 testingUint216;
+        uint224 testingUint224;
+        uint232 testingUint232;
+        uint240 testingUint240;
+        uint248 testingUint248;
+    }
+
+    struct Struct14 {
+        uint256 testingUint256;
+        int8 testingInt8;
+        int16 testingInt16;
+        int24 testingInt24;
+        int32 testingInt32;
+    }
+
+    struct Struct15 {
+        int40 testingInt40;
+        int48 testingInt48;
+        int56 testingInt56;
+        int64 testingInt64;
+        int72 testingInt72;
+    }
+
+    struct Struct16 {
+        int80 testingInt80;
+        int88 testingInt88;
+        int96 testingInt96;
+        int104 testingInt104;
+        int112 testingInt112;
+    }
+
+    struct Struct17 {
+        int120 testingInt120;
+        int128 testingInt128;
+        int136 testingInt136;
+        int144 testingInt144;
+        int152 testingInt152;
+    }
+
+    struct Struct18 {
+        int160 testingInt160;
+        int168 testingInt168;
+        int176 testingInt176;
+        int184 testingInt184;
+        int192 testingInt192;
+    }
+
+    struct Struct19 {
+        int200 testingInt200;
+        int208 testingInt208;
+        int216 testingInt216;
+        int224 testingInt224;
+        int232 testingInt232;
+    }
+
+    struct Struct20 {
+        int240 testingInt240;
+        int248 testingInt248;
+        int256 testingInt256;
+    }
+}
